39fe1f53aee4ce90bd0549cc7c5a60342e7b5b73
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-08-19 22:46:02 +1000

    2d club volume

---
+++ Assets/Scripts/ClubVolume.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClubVolume : MonoBehaviour {
+
+	public float clubMaxVol = 1f;
+	private Transform playerPos = null;
+	private AudioSource clubVol;
+	private float maxDistance;
+	private float currentDistance;
+
+	void Start () {
+		clubVol = GetComponent<AudioSource> ();
+		Invoke ("FindPlayer", 1f); // Had to invoke because player gender swap takes a second
+	}
+
+	void Update () {
+		if (playerPos != null) {
+			currentDistance = transform.position.x - playerPos.transform.position.x;
+			clubVol.volume = Mathf.Lerp (1, 0, currentDistance / maxDistance);
+		}
+	}
+
+	void FindPlayer (){
+		playerPos = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+		maxDistance = transform.position.x - playerPos.transform.position.x;
+	}
+}
