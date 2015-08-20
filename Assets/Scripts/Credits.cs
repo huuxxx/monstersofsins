4a4ad7038a725025576c70ec19bf54bfa1df9ce6
huuxxx <hdowling87@hotmail.com>
2015-08-20 22:46:26 +1000

    Credits Scrolling Text and Input Controls
    Also tweaked the scene a little

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -2,8 +2,27 @@
 using System.Collections;
 
 public class Credits : MonoBehaviour {
-	
-	public void MainMenu () {
-		Application.LoadLevel ("MainMenu");
+
+	public GameObject scrollingText;
+
+	public float scrollSpeed = 0.6f;
+
+	public float endTimer = 45f;
+
+	void FixedUpdate()
+	{
+		scrollingText.transform.position += Vector3.up * scrollSpeed * Time.deltaTime;
+
+		endTimer -= Time.deltaTime;
+
+		if (endTimer <= 0)
+		{
+			Application.LoadLevel ("MainMenu");
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.Mouse0) || Input.GetKeyDown (KeyCode.Escape))
+		{
+			Application.LoadLevel ("MainMenu");
+		}
 	}
 }
